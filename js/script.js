b7a4063affd2013e51b04c9a583ac79e1f826906
moni0217 <moni0217@stud.kea.dk>
2017-02-23 11:29:37 +0100

    loas JSON produktliste and create template-clone for each

--- js/script.js
+++ js/script.js
@@ -2,12 +2,22 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("siden vises");
+
+    // læs produktliste
+    $.getJSON("http://petlatkea.dk/2017/dui/api/productlist?callback=?", visProduktListe);
+
+
     visProdukt();
 }
 
-function visProdukt() {
+function visProduktListe(listen) {
+    console.table(listen);
+    listen.forEach(visProdukt);
+}
+
+function visProdukt(produkt) {
     //klon produkt_template
-    console.log("klon");
+    console.log("produkt");
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
 
     //idsæt data i klon
